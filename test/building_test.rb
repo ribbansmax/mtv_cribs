2fde57b698aa818e5d18e6ba99385e70e5780415
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 11:29:51 -0400

    test_it_can_add_units

--- test/building_test.rb
+++ test/building_test.rb
@@ -17,4 +17,14 @@ class BuildingTest < Minitest::Test
 
     assert_equal [], building.units
   end
+
+  def test_it_can_add_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    assert_equal [unit1, unit2], building.units
+  end
 end
