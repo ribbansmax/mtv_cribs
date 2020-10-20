443b7588aea23863f9617bc4aec596a5a1fc672c
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 11:26:37 -0400

    test_it_can_have_units

--- test/building_test.rb
+++ test/building_test.rb
@@ -11,4 +11,10 @@ class BuildingTest < Minitest::Test
 
     assert_instance_of Building, building
   end
+
+  def test_it_can_have_units
+    building = Building.new
+
+    assert_equal [], building.units
+  end
 end
