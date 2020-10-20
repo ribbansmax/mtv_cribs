6b564da74b61a9c95e36dd47f6d068cd5569230b
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 11:42:17 -0400

    Test_it_can_check_average_rent

--- test/building_test.rb
+++ test/building_test.rb
@@ -47,4 +47,14 @@ class BuildingTest < Minitest::Test
 
     assert_equal ["Aurora", "Tim"], building.renters
   end 
+
+  def test_it_can_check_average_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    assert_equal 1099.5, building.average_rent
+  end
 end
