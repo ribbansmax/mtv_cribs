0a46af7ff5d0e28faa6f76dc2af1c28c2110bb58
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 12:30:04 -0400

    test_rooms_by_renter

--- test/building_test.rb
+++ test/building_test.rb
@@ -150,4 +150,24 @@ class BuildingTest < Minitest::Test
 
     assert_equal expected, building.annual_breakdown
   end
+
+  def test_rooms_by_renter
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+    renter2 = Renter.new("Jessie")
+    unit1.add_renter(renter2)
+    expected = {
+      renter2 => {bathrooms: 1, bedrooms: 1},
+      renter1 => {bathrooms: 2, bedrooms: 2}
+    }
+
+    assert_equal expected, building.rooms_by_renter
+  end
 end
