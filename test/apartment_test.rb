ce9d2de56acb2ef49788418396aa5541dbce0403
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 11:16:14 -0400

    Test_apartment_has_attributes

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -11,6 +11,15 @@ class ApartmentTest < Minitest::Test
     assert_instance_of Apartment, unit1
   end
 
+  def test_apartment_has_attributes
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+    assert_nil unit1.renter
+  end 
   # renter1 = Renter.new("Jessie")
 end
 
