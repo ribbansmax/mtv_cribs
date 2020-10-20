817b56d650f53cba6f239d1f346ac51882953d9d
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 12:06:52 -0400

    test_units_by_number_of_bedrooms

--- test/building_test.rb
+++ test/building_test.rb
@@ -104,4 +104,29 @@ class BuildingTest < Minitest::Test
 
     assert_equal renter2, building.renter_with_highest_rent
   end
+
+  def test_units_by_number_of_bedrooms
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+    renter2 = Renter.new("Jessie")
+    unit1.add_renter(renter2)
+    renter3 = Renter.new("Max")
+    unit3.add_renter(renter3)
+    building.add_unit(unit4)
+    expected = {
+        3 => ["D4" ],
+        2 => ["B2", "C3"],
+        1 => ["A1"]
+      }
+
+    assert_equal expected, building.units_by_number_of_bedrooms
+  end
 end
