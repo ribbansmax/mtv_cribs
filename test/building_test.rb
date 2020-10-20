862cea74c4d7f8e3454c9f9fea4d1cab9df6cf2d
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 11:53:26 -0400

    test_it_can_show_rented_units

--- test/building_test.rb
+++ test/building_test.rb
@@ -61,4 +61,18 @@ class BuildingTest < Minitest::Test
 
     assert_equal 1099.5, building.average_rent
   end
+
+  def test_it_shows_rented_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    assert_equal [], building.rented_units
+  end
 end
