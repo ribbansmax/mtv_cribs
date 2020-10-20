6c43d7f0e7f604b698bf9ec31d63b3c2b8ed173c
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 11:32:32 -0400

    test_it_can_have_renters

--- test/building_test.rb
+++ test/building_test.rb
@@ -27,4 +27,14 @@ class BuildingTest < Minitest::Test
 
     assert_equal [unit1, unit2], building.units
   end
+
+  def test_it_can_have_renters
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    assert_equal [], building.renters
+  end 
 end
