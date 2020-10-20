f3527c51fd5109db4dee20c6dfc7c5978718d207
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 12:41:34 -0400

    test_it_can_check_average_rent_of_rented_units

--- test/building_test.rb
+++ test/building_test.rb
@@ -170,4 +170,20 @@ class BuildingTest < Minitest::Test
 
     assert_equal expected, building.rooms_by_renter
   end
+
+  def test_it_can_check_average_rent_of_rented_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+    renter2 = Renter.new("Tim")
+    unit2.add_renter(renter2)
+
+    assert_equal 1099.5, building.average_rent_of_rented_units
+  end
 end
