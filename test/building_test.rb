f580c3e820533a8a04d984377870499e5c040990
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 11:21:51 -0400

    Create file

---
+++ test/building_test.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/renter"
+require "./lib/apartment"
+require "./lib/building"
+require "pry";
+
+class BuildingTest < Minitest::Test 
+
+end
